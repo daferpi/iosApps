53fbe5ddd20bf26d10a2a57264c4ee057116a598
daferpi <daferpi@gmail.com>
2012-08-27 21:46:14 +0200

    primera version aplicacion penyas del Barça

---
+++ penyasBarsa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  penyasBarsa
+//
+//  Created by asgar on 27/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *listaPenyas;
+
+@end
---
+++ penyasBarsa/ViewController.m
@@ -0,0 +1,85 @@
+//
+//  ViewController.m
+//  penyasBarsa
+//
+//  Created by asgar on 27/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@synthesize listaPenyas;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSArray *tmpListaPenyas = [[NSArray alloc] initWithObjects:@"Penya1",@"Penya2",@"Penya3",@"Penya4",@"Penya5",@"Penya6",@"Penya7",@"Penya8",@"Penya9",@"Penya10", nil];
+    self.listaPenyas = tmpListaPenyas;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+    self.listaPenyas = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.listaPenyas count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [self.listaPenyas objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
